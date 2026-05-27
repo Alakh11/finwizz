be2ebbfdef99c1c4cd6ce9b429eb60268f228ad6
Alakh11 <Alakh1304@gmail.com>
2026-05-27 17:09:16 +0530

    new footer and  navbar added

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,20 +1,15 @@
 jQuery(document).ready(function($) {
-    // 1. Mobile Navbar Toggle
-    $('.navbar-toggle').on('click', function() {
-        $(this).toggleClass('active');
-        $($(this).data('target')).slideToggle();
+    "use strict";
+
+    $('.navbar-nav > li > a:not(.dropdown-toggle)').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
     });
 
-    // 2. Dropdown behavior for mobile
-    $('.menu-item-has-children > a').on('click', function(e) {
-        if ($(window).width() <= 991.98) {
-            e.preventDefault();
-            $(this).siblings('.sub-menu').slideToggle();
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 50) {
+            $('.navbar').addClass('shadow-sm');
+        } else {
+            $('.navbar').removeClass('shadow-sm');
         }
-    });
-
-    // 3. Anchor tag smooth color transition (as per your request)
-    $('a').css({
-        'transition': 'color 1000ms ease 0s, background-color 1500ms ease 0s'
     });
 });
