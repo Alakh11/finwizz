86a8b799dec74189965c06a04aece02269b5150b
Alakh11 <Alakh1304@gmail.com>
2026-05-28 13:04:47 +0530

    navbar issue fixed

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,15 +1,65 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
+
     "use strict";
 
-    $('.navbar-nav > li > a:not(.dropdown-toggle)').on('click', function(){
-        $('.navbar-collapse').collapse('hide');
+    $('.navbar-nav > li > a:not(.dropdown-toggle)')
+        .on('click', function () {
+
+            $('.navbar-collapse')
+                .collapse('hide');
+
+        });
+
+    $(window).scroll(function () {
+
+        if ($(window).scrollTop() > 50) {
+
+            $('.navbar')
+                .addClass('shadow-sm');
+
+        } else {
+
+            $('.navbar')
+                .removeClass('shadow-sm');
+
+        }
+
     });
 
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > 50) {
-            $('.navbar').addClass('shadow-sm');
-        } else {
-            $('.navbar').removeClass('shadow-sm');
+    $('.contact-dropdown').hover(
+
+        function () {
+
+            $(this)
+                .addClass('show');
+
+            $(this)
+                .find('.dropdown-menu')
+                .addClass('show');
+
+        },
+
+        function () {
+
+            $(this)
+                .removeClass('show');
+
+            $(this)
+                .find('.dropdown-menu')
+                .removeClass('show');
+
         }
+
+    );
+
+    $('.contact-link').focus(function () {
+
+        $(this)
+            .closest('.contact-dropdown')
+            .addClass('show')
+            .find('.dropdown-menu')
+            .addClass('show');
+
     });
+
 });
